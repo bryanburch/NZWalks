d18394a25a019870c6eb06bf7eef9217b2dadddb
Bryan <Bryan@BRYAN-PC>
2024-07-28 20:04:49 -0700

    Add project files.

---
+++ NZWalks.API/Models/Domain/Region.cs
@@ -0,0 +1,14 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+    public class Region
+    {
+        public Guid Id { get; set; }
+
+        public string Code { get; set; }
+
+        public string Name { get; set; }
+
+        // RegionImageUrl is nullable
+        public string? RegionImageUrl { get; set; }
+    }
+}

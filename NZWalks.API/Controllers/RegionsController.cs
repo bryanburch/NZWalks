e612b41365e977ea416cb57f1ff62eb3ee67d7ea
bryanburch <bjburch7@gmail.com>
2024-07-28 23:12:44 -0700

    GET Region methods using DTOs

---
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Models.DTO;
+
+namespace NZWalks.API.Controllers
+{
+    // https://localhost:{port}/api/regions
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly NZWalksDbContext dbContext;
+
+        public RegionsController(NZWalksDbContext dbContext) 
+        {
+            this.dbContext = dbContext;
+        }
+
+        // GET ALL REGIONS
+        // GET: https://localhost:{port}/api/regions
+        [HttpGet]
+        public IActionResult GetAll() 
+        {
+            // Get data from database (Domain Model)
+            var regionsDomain = dbContext.Regions.ToList();
+
+            // Map model to DTO
+            var regionsDto = new List<RegionDto>();
+            foreach (var regionDomain in regionsDomain)
+            {
+                regionsDto.Add(new RegionDto()
+                {
+                    Id = regionDomain.Id,
+                    Code = regionDomain.Code,
+                    Name = regionDomain.Name,
+                    RegionImageUrl = regionDomain.RegionImageUrl
+                });
+            }
+
+            // Exposing the DTO instead of the Domain Model
+            return Ok(regionsDto);
+        }
+
+        // GET SINGLE REGION (Get Region by ID)
+        // GET: https://localhost:{port}/api/regions/{id}
+        [HttpGet]
+        [Route("{id:Guid}")] // tells EF to map url variable to action method parameter
+        public IActionResult GetById([FromRoute] Guid id)
+        {
+            // The Find() method can only be used to search on ONLY the primary key
+            //var region = dbContext.Regions.Find(id);
+
+            // The FirstOrDefault() method can be used to search on ANY column attribute
+            var regionDomain = dbContext.Regions.FirstOrDefault(x => x.Id == id);
+
+            if (regionDomain == null)
+            {
+                return NotFound();
+            }
+
+            // Map Domain Model to DTO
+            var regionDto = new RegionDto
+            {
+                Id = regionDomain.Id,
+                Code = regionDomain.Code,
+                Name = regionDomain.Name,
+                RegionImageUrl = regionDomain.RegionImageUrl
+            };
+
+            // Exposing the DTO instead of the Domain Model
+            return Ok(regionDto);
+        }
+    }
+}
---
+++ NZWalks.API/Models/DTO/RegionDto.cs
@@ -0,0 +1,14 @@
+﻿namespace NZWalks.API.Models.DTO
+{
+    // Serves the purpose of decoupling the Region model from the database
+    public class RegionDto
+    {
+        public Guid Id { get; set; }
+
+        public string Code { get; set; }
+
+        public string Name { get; set; }
+
+        public string? RegionImageUrl { get; set; }
+    }
+}

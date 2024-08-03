891715cea5a6f14ae63b4137f45ad186114782bf
bryanburch <bjburch7@gmail.com>
2024-08-03 12:31:45 -0700

    Setup image controller for upload

---
+++ NZWalks.API/Controllers/ImagesController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.DTO;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        // POST: /api/images/upload
+        [HttpPost]
+        [Route("Upload")]
+        public async Task<IActionResult> Upload([FromForm] ImageUploadRequestDto request)
+        {
+            ValidateFileUpload(request);
+
+            if (ModelState.IsValid)
+            {
+                // Use repository to upload image
+            }
+
+            return BadRequest(ModelState);
+        }
+
+        private void ValidateFileUpload(ImageUploadRequestDto request)
+        {
+            // Check extension
+            var allowedExtensions = new string[] { ".jpg", ".jpeg", ".png" };
+
+            if (!allowedExtensions.Contains(Path.GetExtension(request.File.FileName)))
+            {
+                ModelState.AddModelError("file", "Unsupported file type.");
+            }
+
+            // Check size
+            const long MaxFileSizeBytes = 10485760; // 10 Mb
+            if (request.File.Length > MaxFileSizeBytes)
+            {
+                ModelState.AddModelError("file", "File size more than 10Mb, please upload a smaller size file.");
+            }
+        }
+    }
+}

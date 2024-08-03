a9ec934cebf589a1dc3900fa4a176591b2304026
bryanburch <bjburch7@gmail.com>
2024-08-02 22:28:41 -0700

    Apply authorization to Region methods and Swagger auth

--- NZWalks.API/Controllers/RegionsController.cs
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -14,7 +14,6 @@ namespace NZWalks.API.Controllers
     // https://localhost:{port}/api/regions
     [Route("api/[controller]")]
     [ApiController]
-    [Authorize]
     public class RegionsController : ControllerBase
     {
         private readonly IRegionRepository regionRepository;
@@ -30,6 +29,7 @@ namespace NZWalks.API.Controllers
         // GET - ALL REGIONS
         // GET: https://localhost:{port}/api/regions
         [HttpGet]
+        [Authorize(Roles = "Reader")]
         public async Task<IActionResult> GetAll() 
         {
             // Get data from database (Domain Model)
@@ -61,6 +61,7 @@ namespace NZWalks.API.Controllers
         // GET: https://localhost:{port}/api/regions/{id}
         [HttpGet]
         [Route("{id:Guid}")] // tells EF to map url variable to action method parameter
+        [Authorize(Roles = "Reader")]
         public async Task<IActionResult> GetById([FromRoute] Guid id)
         {
             // The Find() method can only be used to search on ONLY the primary key
@@ -82,6 +83,7 @@ namespace NZWalks.API.Controllers
         // POST: https://localhost:{port}/api/regions
         [HttpPost]
         [ValidateModel]
+        [Authorize(Roles = "Writer")]
         public async Task<IActionResult> Create([FromBody] AddRegionRequestDto addRegionRequestDto)
         {
             // Before doing anything we'll check if the data the client gave us
@@ -116,6 +118,7 @@ namespace NZWalks.API.Controllers
         [HttpPut]
         [Route("{id:Guid}")]
         [ValidateModel]
+        [Authorize(Roles = "Writer")]
         public async Task<IActionResult> Update([FromRoute] Guid id, [FromBody] UpdateRegionRequestDto updateRegionRequestDto)
         {
             // Map DTO to Domain Model
@@ -136,6 +139,7 @@ namespace NZWalks.API.Controllers
         // DELETE: https://localhost:{port}/api/regions/{id}
         [HttpDelete]
         [Route("{id:Guid}")]
+        [Authorize(Roles = "Writer,Reader")]
         public async Task<IActionResult> Delete([FromRoute] Guid id)
         {
             // Check if this id exists

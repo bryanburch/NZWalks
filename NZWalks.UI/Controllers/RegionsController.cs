84fdd0bc6ff349c37f740ff7e277699ce16a596d
bryanburch <bjburch7@gmail.com>
2024-08-03 18:34:58 -0700

    UI for adding region

--- NZWalks.UI/Controllers/RegionsController.cs
+++ NZWalks.UI/Controllers/RegionsController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NZWalks.UI.Models;
 using NZWalks.UI.Models.DTO;
+using System.Text;
+using System.Text.Json;
 
 namespace NZWalks.UI.Controllers
 {
@@ -12,6 +15,7 @@ namespace NZWalks.UI.Controllers
             this.httpClientFactory = httpClientFactory;
         }
 
+        [HttpGet]
         public async Task<IActionResult> Index()
         {
             List<RegionDto> response = new List<RegionDto>();
@@ -31,6 +35,37 @@ namespace NZWalks.UI.Controllers
                 // Log exception
             }
             return View(response);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddRegionViewModel model)
+        {
+            var client = httpClientFactory.CreateClient();
+
+            var httpRequestMessage = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri("https://localhost:7173/api/regions"),
+                Content = new StringContent(JsonSerializer.Serialize(model), Encoding.UTF8, "application/json"),
+            };
+
+            var httpResponseMessage = await client.SendAsync(httpRequestMessage);
+            httpResponseMessage.EnsureSuccessStatusCode();
+
+            var response = await httpResponseMessage.Content.ReadFromJsonAsync<RegionDto>();
+
+            if (response != null)
+            {
+                return RedirectToAction("Index", "Regions");
+            }
+
+            return View();
         }
     }
 }
